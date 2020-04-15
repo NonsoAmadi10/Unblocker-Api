909f5efe10f9643cf4c8d21954b40bcca942c118
Houdini <nonsoamadi@aol.com>
2020-04-15 03:09:38 +0100

    user should be able to log in

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,14 @@ class BaseTestCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
         self.client = self.app.test_client()
+        self.data = {
+            'display_name': 'amadi',
+            'email': 'cat5@gmail.com',
+            'password': 'amadijustice345'
+        }
+        """ Seed data """
+        self.client.post('/api/v1/auth/register', json=self.data)
+        
 
 def tearDown(self):
         db.session.remove()
